91ef32774fd6e2a73583d151fce1a113957d5e36
DYLAN ULLRICH <dylan@getullrichordietrying.com>
2022-05-13 20:02:10 -0700

    project section updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Home from "./components/Home";
+import resumeData from "./res_primaryLanguage.json"
+import sharedData from "./portfolio_shared_data.json"
 
 class App extends Component {
 
@@ -16,94 +18,34 @@ class App extends Component {
       resumeData: {},
       sharedData: {},
     };
-  }
-
-  applyPickedLanguage = (pickedLanguage, oppositeLangIconId) => {
-    this.swapCurrentlyActiveLanguage(oppositeLangIconId);
-    document.documentElement.lang = pickedLanguage;
-    var resumePath =
-      document.documentElement.lang === window.$primaryLanguage
-        ? `res_primaryLanguage.json`
-        : `res_secondaryLanguage.json`;
-    this.loadResumeFromPath(resumePath);
-  }
-
-  swapCurrentlyActiveLanguage = (oppositeLangIconId) => {
-    var pickedLangIconId =
-      oppositeLangIconId === window.$primaryLanguageIconId
-        ? window.$secondaryLanguageIconId
-        : window.$primaryLanguageIconId;
-    document
-      .getElementById(oppositeLangIconId)
-      .removeAttribute("filter", "brightness(40%)");
-    document
-      .getElementById(pickedLangIconId)
-      .setAttribute("filter", "brightness(40%)");
-  }
-
-  componentDidMount = () => {
-    this.loadSharedData();
-    this.applyPickedLanguage(
-      window.$primaryLanguage,
-      window.$secondaryLanguageIconId
-    );
-  }
-
-  loadResumeFromPath = (path) => {
-    $.ajax({
-      url: path,
-      dataType: "json",
-      cache: false,
-      success: function (data) {
-        this.setState({ resumeData: data });
-      }.bind(this),
-      error: function (xhr, status, err) {
-        alert(err);
-      },
-    });
-  }
-
-  loadSharedData = () => {
-    $.ajax({
-      url: `portfolio_shared_data.json`,
-      dataType: "json",
-      cache: false,
-      success: function (data) {
-        this.setState({ sharedData: data }, () => document.title = `${this.state.sharedData.basic_info.name}`);
-      }.bind(this),
-      error: function (xhr, status, err) {
-        alert(err);
-      },
-    });
-  }
+  };
 
   render() {
     return (
       <Router>
-        <Header sharedData={this.state.sharedData.basic_info} />
+        <Header sharedData={sharedData.basic_info} />
         <Routes>
           <Route 
             exact path="/" 
             element={<Home
-              resumeData={this.state.resumeData}
-              sharedData={this.state.sharedData}
+              resumeData={resumeData}
+              sharedData={sharedData}
             />}>
           </Route>
           <Route 
             path="/about" 
             element={<About
-              resumeBasicInfo={this.state.resumeData.basic_info}
-              sharedBasicInfo={this.state.sharedData.basic_info}
+              resumeBasicInfo={resumeData.basic_info}
+              sharedBasicInfo={sharedData.basic_info}
             />}>
           </Route>
         </Routes>
         <Footer 
-          sharedBasicInfo={this.state.sharedData.basic_info}
-          applyPickedLanguage={this.applyPickedLanguage} 
+          sharedBasicInfo={sharedData.basic_info}
         />  
       </Router>
     );
   }
-}
+};
 
 export default App;
